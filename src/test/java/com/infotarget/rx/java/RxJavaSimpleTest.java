3f50f010aaad92af6aa9aa977d84439287b4f2eb
tmucha <tmucha@cyfrowypolsat.pl>
2018-01-05 23:36:13 +0100

    adde logging and first test

--- src/test/java/com/infotarget/rx/java/RxJavaSimpleTest.java
+++ src/test/java/com/infotarget/rx/java/RxJavaSimpleTest.java
@@ -1,11 +1,21 @@
 package com.infotarget.rx.java;
 
+import io.reactivex.Observable;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RxJavaSimpleTest {
 
-    @Test
-    public void test() {
+    private Logger log = LoggerFactory.getLogger(RxJavaSimpleTest.class);
 
+    @Test
+    public void simpleObservable() {
+        Observable<String> just = Observable.just("42");
+        just.subscribe(this::print);
+    }
+
+    private <T> void print(T event) {
+        log.info("Got: {}", event);
     }
 }
