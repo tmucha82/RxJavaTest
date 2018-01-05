78cc0785d4ec338df19894478052c98fe15d26eb
tmucha <tmucha@cyfrowypolsat.pl>
2018-01-05 22:33:40 +0100

    Initial commit

---
+++ src/main/java/com/infotarget/rx/java/RxJavaMain.java
@@ -0,0 +1,7 @@
+package com.infotarget.rx.java;
+
+public class RxJavaMain {
+    public static void main(final String[] args) {
+
+    }
+}
---
+++ src/test/java/com/infotarget/rx/java/RxJavaSimpleTest.java
@@ -0,0 +1,11 @@
+package com.infotarget.rx.java;
+
+import org.junit.Test;
+
+public class RxJavaSimpleTest {
+
+    @Test
+    public void test() {
+
+    }
+}
