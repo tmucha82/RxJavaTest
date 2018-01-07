503e1b87b15e967c46f43f2eb86cdaebbc14a2e7
tmucha <tmucha@cyfrowypolsat.pl>
2018-01-07 21:39:11 +0100

    next examples

--- src/test/java/com/infotarget/rx/java/RxJavaSimpleTest.java
+++ src/test/java/com/infotarget/rx/java/RxJavaSimpleTest.java
@@ -5,14 +5,32 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.TimeUnit;
+
 public class RxJavaSimpleTest {
 
     private Logger log = LoggerFactory.getLogger(RxJavaSimpleTest.class);
 
     @Test
-    public void simpleObservable() {
-        Observable<String> just = Observable.just("42");
-        just.subscribe(this::print);
+    public void simpleJustObservable() {
+        Observable
+                .just("42")
+                .subscribe(this::print);
+    }
+
+    @Test
+    public void simpleRangeObservable() {
+        Observable
+                .range(1, 10)
+                .subscribe(this::print);
+    }
+
+    @Test
+    public void simpleIntervalObservable() throws InterruptedException {
+        Observable
+                .interval(1, TimeUnit.SECONDS)
+                .subscribe(this::print);
+
     }
 
     private <T> void print(T event) {
